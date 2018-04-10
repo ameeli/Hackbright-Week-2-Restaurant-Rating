304d2fedf711599222c99fc72b0814ec4d744c89
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 18:01:03 -0700

    Working On Control Flow

--- ratings.py
+++ ratings.py
@@ -1,8 +1,12 @@
 """Restaurant rating lister."""
 
+
 def ask_user_rating():
-    new_name = raw_input ("What is a restaurant name? ")
-    new_rating = raw_input ("What rating would you give this restaurant? ")
+    new_name = raw_input("What is a restaurant name? ")
+    new_rating = int(raw_input("What rating would you give this restaurant? "))
+
+    while new_rating not in range(1, 6):
+        new_rating = int(raw_input("What rating would you give this restaurant? Please enter a number between 1 and 5! "))
 
     return [new_name, new_rating]
 
@@ -36,6 +40,50 @@ def print_ratings(rest_ratings):
         print "%s is rated at %s." % (restaurant, rating)
 
 
-user_input = ask_user_rating()
-rest_ratings = read_ratings('scores.txt')
-print_ratings(rest_ratings)
+# def ask_user_options(files_):
+#     user_choice = int(raw_input('Enter 1 to see all restaurant ratings. \nEnter 2 to add your own rating. \nEnter 3 to quit. \nYour choice: '))
+
+#     while user_choice not in range(1, 4):
+#         user_choice = int(raw_input('Enter 1 to see all restaurant ratings. \nEnter 2 to add your own rating. \nEnter 3 to quit. \nYour choice: '))
+  
+#     if user_choice == 1:
+#         rest_ratings = read_ratings(files_)
+#         print_ratings(rest_ratings)
+
+#     elif user_choice == 2:
+#         user_input = ask_user_rating()
+#         read_ratings('scores.txt')
+
+#     elif user_choice == 3:
+#         return
+
+
+# ask_user_options('scores.txt')
+
+# rest_ratings = read_ratings('scores.txt')
+# print_ratings(rest_ratings)
+
+
+while True:
+
+    # ratings = get_ratings(sys.argv[1])
+
+    # display(ratings)
+
+    user_choice = int(raw_input('Enter 1 to see all restaurant ratings. \nEnter 2 to add your own rating. \nEnter 3 to quit. \nYour choice: '))
+
+    if user_choice == 3:
+        break
+
+    elif user_choice == 1:
+
+        rest_ratings = read_ratings('scores.txt')
+        print_ratings(rest_ratings)
+
+    elif user_choice == 2:
+        ask_user_rating()
+
+    else:
+        print'not valid'
+
+
